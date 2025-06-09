a7cf5266e54769a37141dce7145bd590b3c72c6a
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2025-06-09 21:08:27 +0200

    super

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -122,15 +122,7 @@ const Login = () => {
       
       <div className="mt-8 text-center text-gray-600">
         <p className="text-sm">
-          Développé avec <span className="text-blue-500">💙</span> par{" "}
-          <a 
-            href="https://croissanceconsulting.com/" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="font-medium hover:text-gray-800 underline transition-colors"
-          >
-            Croissance Consulting
-          </a>
+          Développé avec <span className="text-blue-500">💙</span> 
         </p>
       </div>
     </div>
